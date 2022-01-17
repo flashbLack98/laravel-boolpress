0e1f5055f8023eee7017c5ddea50649212003baa
flashbLack98 <npalakuttige98@icloud.com>
2022-01-17 18:51:43 +0100

    complete

---
+++ resources/js/components/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1 class="card">{{ helloMsg }}</h1>
+</template>
+
+<script>
+export default {
+    name: "App",
+    data() {
+        return {
+            helloMsg: "Benvenuto in Matrix",
+        };
+    },
+};
+</script>
+
