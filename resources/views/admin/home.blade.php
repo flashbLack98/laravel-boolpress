a14dd2fe9fa59b940aaca99c9de5a79fdbd4c140
flashbLack98 <npalakuttige98@icloud.com>
2022-01-20 16:27:47 +0100

    commit requested by project manager

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
 <div class="container">
+
+    <h1>HOME DELL'ADMIN</h1>
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
